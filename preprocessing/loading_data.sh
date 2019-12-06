ccb980f0861d91205ec1dfa532c0e7ddc987a3a9
Elkoumy <gamal.elkomy@gmail.com>
2019-12-06 11:30:05 +0200

    uploading data script

--- preprocessing/loading_data.sh
+++ preprocessing/loading_data.sh
@@ -1,20 +1,30 @@
 #loading Data
 
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_BPI_Challenge_2017_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_bpi_model.xml
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_A_BPI_Challenge_2017_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_bpi_model_party_A.xml
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_B_BPI_Challenge_2017_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_bpi_model_party_B.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_BPI_Challenge_2017_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_bpi_model.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_A_BPI_Challenge_2017_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_bpi_model_party_A.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_B_BPI_Challenge_2017_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_bpi_model_party_B.xml
 
 
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_Road_Traffic_Fine_Management_Process_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_traffic_model.xml
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_A_Road_Traffic_Fine_Management_Process_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_traffic_model_party_A.xml
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_B_Road_Traffic_Fine_Management_Process_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_traffic_model_party_B.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_Road_Traffic_Fine_Management_Process_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_traffic_model.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_A_Road_Traffic_Fine_Management_Process_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_traffic_model_party_A.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_B_Road_Traffic_Fine_Management_Process_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_traffic_model_party_B.xml
 
 
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_Hospital_log_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_hospital_model.xml
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_A_Hospital_log_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_hospital_model_party_A.xml
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_B_Hospital_log_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_hospital_model_party_B.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_Hospital_log_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_hospital_model.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_A_Hospital_log_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_hospital_model_party_A.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_B_Hospital_log_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_hospital_model_party_B.xml
 
 
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_CreditRequirement_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_credit_model.xml
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_A_CreditRequirement_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_credit_model_party_A.xml
-sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_B_CreditRequirement_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_credit_model_party_B.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_CreditRequirement_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_credit_model.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_A_CreditRequirement_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_credit_model_party_A.xml
+#sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/sharemind/datasets/reduced_party_B_CreditRequirement_MPC.csv --model /home/debian/sharemind/Github/SecureMPCBPM/data_and_preprocessing/reduced_credit_model_party_B.xml
+
+
+sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/shareprom/shareprom/CCC19_MPC.csv --model /home/debian/shareprom/shareprom/models/CCC19_model.xml
+sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/shareprom/shareprom/party_A_CCC19_MPC.csv --model /home/debian/shareprom/shareprom/models/CCC19_model_party_A.xml
+sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/shareprom/shareprom/party_B_CCC19_MPC.csv --model /home/debian/shareprom/shareprom/models/CCC19_model_party_B.xml
+
+
+sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/shareprom/shareprom/BPI13_MPC.csv --model /home/debian/shareprom/shareprom/models/bpi13_model.xml
+sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/shareprom/shareprom/party_A_BPI13_MPC.csv --model /home/debian/shareprom/shareprom/models/bpi13_model_party_A.xml
+sudo sharemind-csv-importer --conf /etc/sharemind/client.conf --mode overwrite --csv /home/debian/shareprom/shareprom/party_B_BPI13_MPC.csv --model /home/debian/shareprom/shareprom/models/bpi13_model_party_B.xml
