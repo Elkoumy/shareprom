49f0cc1e37807ea0d1e001bf6db87a940fd48613
Gamal Elkoumy <gamal.elkomy@gmail.com>
2019-12-16 12:51:58 +0200

    Create run_experiment.sh

---
+++ experiment/run_experiment.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo -n CCC19>tbl_name
+sudo sharemind-runscript main.sb --str=TBL --str= --str=string --file=tbl_name --str=CHUNKS --str="" --str=uint64 --size=8 --uint64=8 --str=EVENTA --str pd_shared3p --str=uint64 --size=8 --uint64=27 --str=EVENTB --str pd_shared3p --str=uint64 --size=8 --uint64=12sudo sharemind-runscript main.sb --str=TBL --str= --str=string --file=tbl_name --str=CHUNKS --str="" --str=uint64 --size=8 --uint64=8 --str=EVENTA --str pd_shared3p --str=uint64 --size=8 --uint64=27 --str=EVENTB --str pd_shared3p --str=uint64 --size=8 --uint64=12
