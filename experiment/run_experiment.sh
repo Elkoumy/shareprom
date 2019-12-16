5f49223830f9d033e09f20f2f831674b3dfcbeb7
root <root@gamal-debian-1.cloud.ut.ee>
2019-12-16 15:35:04 +0000

    merging#

--- experiment/run_experiment.sh
+++ experiment/run_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -n CCC19>tbl_name
-sudo sharemind-runscript main.sb --str=TBL --str= --str=string --file=tbl_name --str=CHUNKS --str="" --str=uint64 --size=8 --uint64=8 --str=EVENTA --str pd_shared3p --str=uint64 --size=8 --uint64=27 --str=EVENTB --str pd_shared3p --str=uint64 --size=8 --uint64=12sudo sharemind-runscript main.sb --str=TBL --str= --str=string --file=tbl_name --str=CHUNKS --str="" --str=uint64 --size=8 --uint64=8 --str=EVENTA --str pd_shared3p --str=uint64 --size=8 --uint64=27 --str=EVENTB --str pd_shared3p --str=uint64 --size=8 --uint64=12
+sudo sharemind-runscript main.sb --str=TBL --str= --str=string --file=tbl_name --str=CHUNKS --str="" --str=uint64 --size=8 --uint64=8 --str=EVENTA --str pd_shared3p --str=uint64 --size=8 --uint64=27 --str=EVENTB --str pd_shared3p --str=uint64 --size=8 --uint64=12
 
 
 echo -n reduced_traffic>tbl_name
-sudo sharemind-runscript main.sb --str=TBL --str= --str=string --file=tbl_name --str=CHUNKS --str="" --str=uint64 --size=8 --uint64=8 --str=EVENTA --str pd_shared3p --str=uint64 --size=8 --uint64=27 --str=EVENTB --str pd_shared3p --str=uint64 --size=8 --uint64=12sudo sharemind-runscript main.sb --str=TBL --str= --str=string --file=tbl_name --str=CHUNKS --str="" --str=uint64 --size=8 --uint64=8 --str=EVENTA --str pd_shared3p --str=uint64 --size=8 --uint64=15 --str=EVENTB --str pd_shared3p --str=uint64 --size=8 --uint64=5
+sudo sharemind-runscript main.sb --str=TBL --str= --str=string --file=tbl_name --str=CHUNKS --str="" --str=uint64 --size=8 --uint64=8 --str=EVENTA --str pd_shared3p --str=uint64 --size=8 --uint64=15 --str=EVENTB --str pd_shared3p --str=uint64 --size=8 --uint64=5
