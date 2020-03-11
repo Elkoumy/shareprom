d12b45f17f156dc964d65c94091c7a9869cd0a06
root <root@gamal-debian-1.cloud.ut.ee>
2020-03-11 16:20:26 +0000

    comminting experiment

--- experiment/controlled_experiment.sh
+++ experiment/controlled_experiment.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 dataset=(
-  cases_10000
-  cases_100000
-  cases_1000000
+#  cases_10000
+#  cases_100000
+#  cases_1000000
   activities_10
   activities_50
   activities_100
-  max_100
-  max_1000
+#  max_100
+#  max_1000
 )
 eventA=(
-  5
-  5
-  5
+#  5
+#  5
+#  5
   73
   77
   91
-  74
-  622
+#  74
+#  622
 )
 
 eventB=(
-  4
-  4
-  4
+#  4
+#  4
+#  4
   73
   69
   69
-  64
-  510
+#  64
+#  510
 )
 
 
@@ -40,9 +40,9 @@ do
 for index in ${!dataset[*]}; do 
   echo "***********************************************************"
   echo "Current Dataset is ${dataset[$index]}"
-  sudo nohup nethogs -t |tee  /home/debian/shareprom/shareprom/experiment/logs/server1/${dataset[$index]}_$chunk>temp &
-    ssh debian@SharemindServer2 sudo nohup nethogs -t |tee  /home/debian/shareprom/shareprom/experiment/logs/server2/${dataset[$index]}_$chunk>temp &
-    ssh debian@SharemindServer3 sudo nohup nethogs -t |tee  /home/debian/shareprom/shareprom/experiment/logs/server3/${dataset[$index]}_$chunk>temp &
+  sudo nohup nethogs -t |tee  /home/debian/shareprom/shareprom/experiment/logs/server1/${dataset[$index]}_$chunk>/home/debian/shareprom/shareprom/experiment/logs/server1/${dataset[$index]}_$chunk &
+    ssh debian@SharemindServer2 sudo nohup nethogs -t |tee  /home/debian/shareprom/shareprom/experiment/logs/server2/${dataset[$index]}_$chunk>/home/debian/shareprom/shareprom/experiment/logs/server2/${dataset[$index]}_$chunk &
+    ssh debian@SharemindServer3 sudo nohup nethogs -t |tee  /home/debian/shareprom/shareprom/experiment/logs/server3/${dataset[$index]}_$chunk>/home/debian/shareprom/shareprom/experiment/logs/server3/${dataset[$index]}_$chunk &
 
 sudo sharemind-runscript main.sb --str=TBL --str= --str=string --str="${dataset[$index]}" --str=CHUNKS --str="" --str=uint64 --size=8 --uint64=$chunk --str=EVENTA --str pd_shared3p --str=uint64 --size=8 --uint64=${eventA[$index]} --str=EVENTB --str pd_shared3p --str=uint64 --size=8 --uint64=${eventB[$index]}
 
