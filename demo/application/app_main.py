f13ade1f4b938f7864f11a08a1a66d7772c0d5a2
root <root@sharemind-sdk.sharemind-sdk>
2020-04-17 05:39:37 -0400

    fixing parsing

--- demo/application/app_main.py
+++ demo/application/app_main.py
@@ -5,10 +5,10 @@ from parse_results import parse_results
 input_dir=r"/home/sharemind/shareprom/demo/application/data"
 output_dir=r"/home/sharemind/shareprom/demo/application/data"
 log_dir= r"/home/sharemind/shareprom/demo/application/out3.log"
-dataset_name= "max_10"
+dataset_name= "activities_10"
 no_of_chunks=1
-event_a=10
-event_b=9
+event_a=23
+event_b=22
 
 #preprocessing(input_dir, output_dir, dataset_name)
 upload(output_dir,dataset_name)
