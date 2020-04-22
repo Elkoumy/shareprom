c988331cffb5c8fba7306e2b2b88535010b217e9
Stephan Fahrenkrog-Petersen <samadeusfp@gmail.com>
2020-04-22 16:16:08 +0200

    Handling new output structure in anonymization and integrating basic handling of time

--- demo/application/add_differential_privacy_dfg.py
+++ demo/application/add_differential_privacy_dfg.py
@@ -1,7 +1,15 @@
 import diffprivlib.mechanisms as privacyMechanisms
 import sys
 
-def add_noise_to_dfg(DFG_matrix,epsilon):
+def add_noise_to_dfg(DFG_matrix,time_matrix,epsilon):
     laplace_mechanism = privacyMechanisms.LaplaceBoundedDomain()
     laplace_mechanism.set_sensitivity(1).set_bounds(0,sys.maxsize).set_epsilon(epsilon)
-    return [int(round(laplace_mechanism.randomise(x))) for x in DFG_matrix]
+    anonymizied_DFG_matrix = list()
+    anonymizied_time_matrix = list()
+    for i in range(0,len(DFG_matrix)):
+        anonymizied_DFG_matrix.append(int(round(laplace_mechanism.randomise(DFG_matrix[i]))))
+        if anonymizied_DFG_matrix[i] == 0:
+            anonymizied_time_matrix.append(0)
+        else:
+            anonymizied_time_matrix.append(time_matrix[i])
+    return anonymizied_DFG_matrix, anonymizied_time_matrix
