31ea27671de95b523b5be8d92fd844cd4b5aad7a
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-04-27 13:38:57 +0300

    adding buttons icons and University Logo

--- demo/application/GUI_application.py
+++ demo/application/GUI_application.py
@@ -7,9 +7,10 @@ from PIL import Image, ImageTk
 
 
 
-root = tk.Tk() # the object that contains everything
+root = tk.Tk( ) # the object that contains everything
 # we add a canvas to put components on it and control the size
-canvas = tk.Canvas(root, height= 700, width= 700, bg="#263D42")
+# canvas = tk.Canvas(root, height= 700, width= 700, bg="#263D42")
+root.config( bg="white")
 
 file=[]
 def add_xes():
@@ -17,7 +18,11 @@ def add_xes():
     filename= filedialog.askopenfilename(initialdir=r"DFG_log", title="Select File",
                                          filetypes=(("XES","*.xes"), ("all files", "*.*")))
     file.append(filename)
-    print(filename)
+
+    if list_box.get(0)=="No Files Selected. Please add a file to be processed!":
+        list_box.delete(0)
+    list_box.insert(len(file), filename)
+
 
 def run_dfg():
     #call the functions here
@@ -25,18 +30,18 @@ def run_dfg():
     drawing = svg2rlg("manufacurer.svg")
     renderPM.drawToFile(drawing, "dfg.png", fmt="PNG")
     img = Image.open('dfg.png')
-    pimg = ImageTk.PhotoImage(img)
-    size = img.size
 
-    root.title("viewer")
+    win = tk.Toplevel()
+    win.wm_title("Window")
 
-    view = Image(root)
-    view.pack()
-    view.setimage(img)
-    view.config(width=img.size[0], height=img.size[1])
-    canvas.create_image(0, 0, anchor='nw', image=pimg)
-    canvas.pack()
+    l = tk.Label(win, text="")
+    img = tk.PhotoImage(file="dfg.png")
+    l.config(image=img)
+    l.image = img
+    l.grid(row=0, column=0)
+
     return
+
 
 
 
@@ -44,26 +49,42 @@ def run_dfg():
 #frame= tk.Frame(root, bg="white")
 #frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-#labels
-# label= tk.Label(frame, text="asdfadf")
-# label.pack()
+
+#the shareprom title
+title= tk.Label(root, text="Shareprom-  a Secure Multi-party computation system for Inter-organizational Process Mining", font='Helvetica 14 bold', bg="white")
+title.pack()
+
 
 #making buttons (docked to root) the function on click is add_app
-openFile = tk.Button(root, text="Add XES file", padx=10, pady=5, fg="white", bg="#263D42", command=add_xes)
+openFile = tk.Button(root, text = 'Click Me !',    command=add_xes,  bg="white")
+img = tk.PhotoImage(file=r"GUI_images\add_file.png")
+openFile.config(image=img)
 openFile.pack()
+openFile.image=img
 
 
-build_dfg = tk.Button(root, text="Build DFG", padx=10, pady=5, fg="white", bg="#263D42" , command = run_dfg)
+### list view for listing the files
+list_label = tk.Label(root, text="Uploaded File", font='Helvetica 10 bold', bg="white")
+list_box = tk.Listbox(root,width=50, height=20)
+list_box.insert(1,"No Files Selected. Please add a file to be processed!")
+list_label.pack()
+list_box.pack()
+
+
+#### build dfg button
+build_dfg = tk.Button(root, text="Build DFG", command = run_dfg,  bg="white")
+img = tk.PhotoImage(file=r"GUI_images\calculate_dfg.png")
+# build_dfg.grid(row=0)
+build_dfg.config(image=img)
 build_dfg.pack()
+build_dfg.image=img
 
 
-
-
-
-
-
-
-
-
+### University of Tartu Logo
+tartu = tk.Label(root, text="", bg="white")
+img = tk.PhotoImage(file=r"GUI_images\tartu.png")
+tartu.config(image=img)
+tartu.image = img
+tartu.pack()
 #start the application GUI
 root.mainloop()
