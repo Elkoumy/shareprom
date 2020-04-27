bb6e7f57c221236e706d78aac775d39a7d6a8b08
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-04-27 18:18:53 +0300

    fix dir

--- demo/application/parse_results.py
+++ demo/application/parse_results.py
@@ -2,4 +2,4 @@
 import os
 def parse_results(dir_to_log):
     print("parsing the results")
-    os.system('cat '+dir_to_log+' > FG_log/DFG.log | python argument-stream-decipher.py')
+    os.system('cat '+dir_to_log+' | python argument-stream-decipher.py > DG_log/DFG.log')
